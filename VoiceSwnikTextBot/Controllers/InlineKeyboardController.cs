f9f16e2913e3c1bf47f9ad4d1755660636bf1fbc
Ведерников Сергей <swnike@mail.ru>
2025-01-12 20:37:34 +0500

    Обработка кнопок и сохранение данных

--- VoiceSwnikTextBot/Controllers/InlineKeyboardController.cs
+++ VoiceSwnikTextBot/Controllers/InlineKeyboardController.cs
@@ -5,22 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using VoiceSwnikTextBot.Services;
 
 namespace VoiceSwnikTextBot.Controllers
 {
     internal class InlineKeyboardController
     {
+        private readonly IStorage _memoryStorage;
         private readonly ITelegramBotClient _telegramClient;
-        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
         {
             _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
         }
 
         public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
         {
-            Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
+            if (callbackQuery?.Data == null)
+                return;
 
-            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку", cancellationToken: ct);
+            // Обновление пользовательской сессии новыми данными
+            _memoryStorage.GetSession(callbackQuery.From.Id).LanguageCode = callbackQuery.Data;
+
+            // Генерим информационное сообщение
+            string languageText = callbackQuery.Data switch
+            {
+                "ru" => " Русский",
+                "en" => " Английский",
+                _ => String.Empty
+            };
+
+            // Отправляем в ответ уведомление о выборе
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $"<b>Язык аудио - {languageText}.{Environment.NewLine}</b>" +
+                $"{Environment.NewLine}Можно поменять в главном меню.", cancellationToken: ct, parseMode: ParseMode.Html);
+
+            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            //await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку {callbackQuery.Data}", cancellationToken: ct);
         }
     }
 }
